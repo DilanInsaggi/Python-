609b23478e7f3ca381484488dbf4a54fc82a1f24
DilanInsaggi <dilan.fuentes.silva@alumnos.sip.cl>
2023-03-15 13:27:44 -0300

    13:27

---
+++ python/fundamentals/fundamentals/bucles.for.py
@@ -0,0 +1,47 @@
+
+for i in range (151):
+    print(i)
+
+
+
+
+for x in range (0 , 1005 , 5):
+    print(x)
+
+
+
+for i in range (1, 101):
+    if i % 10 == 0:
+        print ("Coding Dojo")
+    elif i % 5 == 0: 
+        print("Coding")
+    else:
+        print(i)
+
+
+
+suma = 0
+for i in range(1, 500001, 2):
+    suma += i 
+print(suma)
+
+
+
+for i in range(2018, 0, -4):
+    print(i)
+
+
+
+        
+lowNum = 2
+highNum = 500
+mult = 10
+
+
+
+for i in range (lowNum, highNum + 1):
+    if i % mult == 0:
+        print(i)
+
+
+        
